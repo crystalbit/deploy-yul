37e17b5c1dc02cec35cfde5bd610d5c99fc9f13f
Dima <crystalbit@mail.ru>
2023-11-13 00:27:49 +0300

    Update YulDeployer.sol - add broadcast

--- contracts/YulDeployer.sol
+++ contracts/YulDeployer.sol
@@ -23,6 +23,7 @@ contract YulDeployer is Test {
 
     ///@notice deploy the bytecode with the create instruction
     address deployedAddress;
+    vm.broadcast();
     assembly {
       deployedAddress := create(0, add(bytecode, 0x20), mload(bytecode))
     }
